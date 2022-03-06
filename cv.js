689ad47dea838f625faa5390528ed82a51833ee1
vaibhavsharma1234 <96341386+vaibhavsharma1234@users.noreply.github.com>
2022-03-06 19:44:58 +0530

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,8 @@
+function BellBoy (name, age, hasWorkPermit, language) {
+    this.name = name;
+    this.age = age;
+    this.hasWorkPermit = hasWorkPermit;
+    this.language = language;
+  
+  }
+  var bellBoy1 = new BellBoy ("timmy", 19,true ,["french","english"]);
